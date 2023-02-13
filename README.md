4730479a47dabf3a99deb0ba33e43a73ff44227a
Bastian1110 <chebimora02@gmail.com>
2023-02-13 12:24:31 -0600

    Upadte Readme

--- README.md
+++ README.md
@@ -1,38 +1,15 @@
-# create-svelte
+# Word 2 Emotions
 
-Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
+This is a little personal project to experiment a little bit with WASM, Go and Sevelte.
+The objective is to create a SPA that can use the WebSpeech API to recognize speech, then
+convert the user voice and try to pedict what they are filling with NLP.
 
-## Creating a project
+## Tech Stack
 
-If you're seeing this, you've probably already done this step. Congrats!
+- SvelteKit
+- TailwindCSS
+- Go
 
-```bash
-# create a new project in the current directory
-npm create svelte@latest
+## Deployment
 
-# create a new project in my-app
-npm create svelte@latest my-app
-```
-
-## Developing
-
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
-
-```bash
-npm run dev
-
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
-```
-
-## Building
-
-To create a production version of your app:
-
-```bash
-npm run build
-```
-
-You can preview the production build with `npm run preview`.
-
-> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+I'm working in making an automated CI/CD with GitHub Actions!
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-// @ts-nocheck
+     // @ts-nocheck
     let recognition;
-    let text;
+    let text = "Sup?";
     try {
         const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
         recognition = new SpeechRecognition();
@@ -11,43 +11,35 @@
 
     recognition.interimResults = true;
     recognition.lang = 'es';
-    let recognitionStarted = false;
+
     const activateRecognition = () => {
-        if (recognitionStarted) {
-            recognition.stop();
-            recognitionStarted = !recognitionStarted;
-            console.log("Recognition stoped")
-            return;
-        }
-        if (!recognitionStarted) {
-            recognition.start();
-            recognitionStarted = !recognitionStarted;
-            console.log("Recognition started")
-            return;
-        }
+        recognition.start();
+        console.log("Recognition started")
+        return;
     }
 
+    const deactivateRecognition = () => {
+        recognition.stop();
+        console.log("Recognition stoped")
+        return;
+    }
     recognition.onresult = (event) => {
         text = event.results[0][0].transcript;
-        console.log(res)
     }
 </script>
 
-<h1 class="font-bold text-6xl m-10 text-[#affaff]">Speech Test</h1>
+<h1 class="font-bold text-6xl m-10 text-[#affaff]">Words to Emotions</h1>
 <div class="text-4xl m-10 text-[#d4f9fc]">
     <p>{text}</p>
 </div>
 
 <div class="grid place-items-center">
-    <button on:click={activateRecognition} class="trasnition duration-300 bg-[#6653bef7] p-6 rounded-full hover:scale-110 hover:bg-[#5d48bdf7] active:bg-[#663894f7]"> 
+    <button on:mousedown={activateRecognition} on:mouseup={deactivateRecognition} class="trasnition duration-300 bg-[#6653bef7] p-6 rounded-full hover:scale-110 hover:bg-[#5d48bdf7] active:bg-[#7a43b4f7]"> 
         <svg class="h-16 w-16 text-[#affaff]"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"/>
         </svg>
     </button>
 </div>
-
-
-
 
 <style lang="postcss">
     :global(html){
