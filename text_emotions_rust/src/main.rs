4977d0f93875166c036fae3a147290e620f0660d
Bastian1110 <chebimora02@gmail.com>
2023-02-16 19:43:46 -0600

    NLP Working :D

--- text_emotions_rust/src/main.rs
+++ text_emotions_rust/src/main.rs
@@ -1,8 +1,12 @@
 use csv;
 use dict::{Dict, DictIface};
+use linfa::{metrics::ToConfusionMatrix, traits::Fit, traits::Predict, DatasetBase};
+use linfa_bayes::GaussianNb;
+//use linfa_logistic::LogisticRegression;
 use linfa_preprocessing::CountVectorizer;
 use ndarray::prelude::*;
 
+//Dataframe for reading the emotion dataset
 struct DataFrame {
     index: Vec<u128>,
     emotion: Vec<usize>,
@@ -51,11 +55,52 @@ impl DataFrame {
 }
 
 fn main() {
-    let data = DataFrame::read_csv("./dataset/test.csv");
-    let vec_training_sentences: Vec<&str> = data.clean_text.iter().map(|x| &**x).collect();
-    let arr_training_sentences = Array::from_vec(vec_training_sentences);
+    //Importing the dataset from csv
+    let data = DataFrame::read_csv("./dataset/emotions_clean.csv");
+
+    //Vectorizing the text from dataset
+    let vec_sentences: Vec<&str> = data.clean_text.iter().map(|x| &**x).collect();
+    let arr_sentences = Array::from_vec(vec_sentences);
     println!(
-        "Targets : {:?} \n Docs : {:?}",
-        data.emotion, data.clean_text
-    )
+        "Targets : {:?}  Docs : {:?}",
+        data.emotion.len(),
+        data.clean_text.len()
+    );
+    let vectorizer = CountVectorizer::params().fit(&arr_sentences).unwrap();
+    println!(
+        "We obtain a vocabulary with {} entries",
+        vectorizer.nentries()
+    );
+
+    println!("Emotion Table");
+    for o in data.emotions_map {
+        println!("{} - {}", o.key, o.val);
+    }
+
+    //Making the training datset with records and targets
+    let records = vectorizer.transform(&arr_sentences).to_dense();
+    let records = records.mapv(|c| c as f32);
+
+    let targets = Array1::from_shape_vec(data.emotion.len(), data.emotion).unwrap();
+
+    let (train, valid) = DatasetBase::new(records, targets)
+        .split_with_ratio(0.9)
+        .into();
+
+    //Making the model
+    let model = GaussianNb::params().fit(&train).unwrap();
+    let training_prediction = model.predict(&train);
+
+    //Acurracy
+    let cm = training_prediction.confusion_matrix(&train).unwrap();
+    let accuracy = cm.f1_score();
+    println!("The fitted model has a training f1 score of {}", accuracy);
+
+    let test_string = array!["I'm so borred right now, I want to go home"];
+    let test_array = vectorizer.transform(&test_string).to_dense();
+    let test_array = test_array.mapv(|c| c as f32);
+
+    println!("Test : {}", test_string);
+    let test_prediction = model.predict(&test_array);
+    println!("{:?} Prediction of last entry", test_prediction.first());
 }
